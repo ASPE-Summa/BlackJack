9f17618ae072ed09565b7207738c9b50961af31d
Peter Askey <aspe@summacollege.nl>
2022-01-23 09:54:10 +0100

    fixed scores of face cards

--- functions.php
+++ functions.php
@@ -123,23 +123,28 @@ function hit(string $player = PLAYER): void
 function calculateTotal(array $hand): int
 {
     $total = 0;
+    $aces = 0;
     foreach ($hand as $card) {
         $firstCharacter = substr($card, 0, 1);
         if (is_numeric($firstCharacter)) {
             $total += $firstCharacter;
-        } else if ($firstCharacter == 'a') {
-            $total += 1;
-        } else if ($firstCharacter == 't') {
+        } 
+        else if (in_array($firstCharacter, ['t', 'j', 'q', 'k'])) {
             $total += 10;
-        } else if ($firstCharacter == 'j') {
-            $total += 11;
-        } else if ($firstCharacter == 'q') {
-            $total += 12;
-        } else if ($firstCharacter == 'k') {
-            $total += 13;
+        }
+        else if($firstCharacter == 'a'){
+            $aces++;
         }
     }
 
+    for($i = 0; $i < $aces; $i++){
+        if(($total + 11) <= 21){
+            $total += 11;
+        }
+        else{
+            $total += 1;
+        }
+    }
     return $total;
 }
 
